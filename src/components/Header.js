f81eb79a26859873d764936ed3e2ae5c2ce56dbd
Livia Welschinger <livia.welschinger@bdr.de>
2021-08-05 10:35:46 +0200

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+  return (
+    <header className='header'>
+      <h1 >{title}</h1>
+      <Button backgroundColor='green' text='Add'></Button>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+  color: 'red',
+  backgroundColor: 'black'
+}
+export default Header;
